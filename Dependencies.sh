75401d312e540a5115dee0859c83ca5dd5038325
Danielnotuser <daniil.listikov@gmail.com>
2026-04-09 20:28:38 +0300

    update to python3.12

--- Dependencies.sh
+++ Dependencies.sh
@@ -7,14 +7,16 @@ python3 -m venv $1
 
 source $1/bin/activate
 
-pip install \
-	numpy==1.20\
-	opencv-python==4.5.4.60 \
-	pygame==2.1.0 \
-	scipy==1.7.3 \
-	jupyter==1.0.0 \
-  numba==0.54.1\
-  nose==1.3.7\
-  parameterized==0.8.1\
-  sklearn \
-  Pillow
+pip install --default-timeout=100 \
+  setuptools \
+	numpy \
+	opencv-python \
+	pygame \
+	scipy \
+	jupyter \
+  numba \
+  nose \
+  parameterized \
+  scikit-learn \
+  Pillow \
+  typeguard
